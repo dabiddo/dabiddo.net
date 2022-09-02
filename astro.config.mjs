0d5a380b09a000b9bdafab2923dd138264e3c838
dabiddo <dabiddo@gmail.com>
2022-09-02 05:12:23 +0000

    updated config file for site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dabiddo.net',
+  site: 'https://www.dabiddo.net',
   integrations: [mdx(), sitemap(), tailwind()]
 });
