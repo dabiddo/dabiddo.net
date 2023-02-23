653cce7a9052ae4f960833084cfa5b056a3e559c
Dabiddo <dabiddo@gmail.com>
2023-02-23 01:24:27 +0000

    missing description from collection type

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,6 +5,7 @@ import { z, defineCollection } from 'astro:content';
 const blogCollection = defineCollection({
     schema: z.object({
       title: z.string(),
+      description: z.string(),
       pubDate: z.date(),
       tags: z.array(z.string()),
       image: z.object({
