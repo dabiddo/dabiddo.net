f54fd0f03928b06895c2ad0489eb1b762d00eb7f
David Torres <dabiddo@gmail.com>
2024-12-14 16:02:14 +0000

    delete old v4 files, replaced with the whole astro 5 framework for clean slate

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
-/**import { defineConfig } from 'astro/config';
+// @ts-check
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.dabiddo.net',
-  integrations: [mdx(), sitemap()]
-});*/
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://www.dabiddo.net',
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
--- src/config.ts
+++
@@ -1,7 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Dabiddo\'s Blog';
-export const SITE_DESCRIPTION = 'Welcome to my Blog!';
-export const AUTHOR = 'Dabiddo';
-
